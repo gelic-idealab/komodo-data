4f0fa75950e178c5c62adb8bd2caad00ce1afafc
Wallace <rwwalla2@illinois.edu>
2021-09-01 12:01:03 -0500

    dummy data folders, test sql

---
+++ test_data/test_sql.sql
@@ -0,0 +1,9 @@
+-- set the appropriate database (ie. `use komodo;`)
+-- insert a dummy capture record
+INSERT INTO komodo.captures (`capture_id`, `session_id`, `start`, `end`) VALUES ('126_1630443513898', 126, 1630443513898, 1630443719921);
+
+-- reset the flag on the dummy record
+UPDATE komodo.captures SET processed = NULL WHERE capture_id = '126_1630443513898';
+
+-- querying the message JSON field 
+SELECT * from komodo.data where message->'$.entityType' = 0 ORDER BY id DESC;
