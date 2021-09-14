f6ff279615cbbb1453887defde524f225a6eb6bc
Brandon Dang <bcdang2@illinois.edu>
2021-09-14 02:10:20 -0500

    add note for workbench

--- test_data/test_sql.sql
+++ test_data/test_sql.sql
@@ -1,3 +1,5 @@
+-- SET SQL_SAFE_UPDATES = 0; -- enable this if using MySQL Workbench
+
 -- set the appropriate database (ie. `use komodo;`)
 -- insert a dummy capture record
 INSERT INTO komodo.captures (`capture_id`, `session_id`, `start`, `end`) VALUES ('126_1630443513898', 126, 1630443513898, 1630443719921);
@@ -7,4 +9,3 @@ UPDATE komodo.captures SET processed = NULL WHERE capture_id = '126_163044351389
 
 -- querying the message JSON field 
 SELECT * from komodo.data where message->'$.entityType' = 0 ORDER BY id DESC;
-
