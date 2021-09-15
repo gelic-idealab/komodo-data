e71a623f046877d6dcf13ffb96ae2f50c55107e1
Wendy Zhu <wheeendiii@Wendys-MacBook-Pro.local>
2021-09-15 13:56:09 -0500

    modified example queries and insert outputs to new table

--- process.py
+++ process.py
@@ -2,8 +2,10 @@ import os
 import sys
 import time
 import json
+from numpy.core.defchararray import count
 import pandas as pd
 import numpy as np
+import unittest as ut
 from sqlalchemy import create_engine, text, types
 
 # import db configs
@@ -45,46 +47,100 @@ def check_for_unprocessed_captures():
     return ready
 
 def aggregate_interaction_type(session_id, interaction_type):
-    # aggregate by interaction types 
-    with engine.connect() as conn:
-        query = text("""
-        SELECT count(*) 
-        FROM data
-        WHERE message->'$.interactionType' = :interaction_type and session_id = :session_id;
-        """
-        )
+    try: 
+        # aggregate by interaction types 
+        with engine.connect() as conn:
+            query = text("""
+            DROP TABLE IF EXISTS `komodo`.`aggregate_interaction`;
+            """
+            )
 
-        result = conn.execute(query,{"session_id":session_id, "interaction_type":interaction_type})
-        count = [r[0] for r in result]
+            conn.execute(query)
+            
+            query = text("""
+            CREATE TABLE `komodo`.`aggregate_interaction` (client_id int not null,
+            primary key (client_id),
+            interaction_count int not null);
+            """
+            )
+
+            conn.execute(query)
+
+            query = text("""
+            INSERT INTO aggregate_interaction 
+            SELECT client_id, count(message) as interaction_count
+            FROM data
+            WHERE message->'$.interactionType' = :interaction_type and session_id= :session_id
+            group by client_id;
+            """
+            )
+
+            conn.execute(query,{"session_id":session_id, "interaction_type":interaction_type})
+    except:
+        return False
         
-        # uncomment this to test the output 
-        # print(query)
-        # print(count)
-        # sys.exit() 
+    # uncomment this to test the output 
+    # query = text("""
+    # SELECT * 
+    # FROM aggregate_interaction;
+    # """
+    # )
+    # result = conn.execute(query)
+    # count = [r[1] for r in result]
+    # sum = np.sum(count)
+    # print(sum == 13)
+    # sys.exit() 
         
-
-    return count
-
+    return True
 
 def aggregate_user(session_id,client_id):
     # aggregate by users
-    with engine.connect() as conn:
-        query = text("""
-        SELECT count(*) 
-        FROM data
-        WHERE message->'$.clientId' = :client_id and session_id = :session_id;
-        """
-        )
+    try:
+        with engine.connect() as conn:
+            query = text("""
+            DROP TABLE IF EXISTS `komodo`.`aggregate_user`;
+            """
+            )
 
-        result = conn.execute(query,{"session_id":session_id, "client_id":client_id})
-        count = [r[0] for r in result]
+            conn.execute(query)
+            query = text("""
+            CREATE TABLE if not exists aggregate_user 
+            (
+            entity_type int not null,
+            primary key (entity_type),
+            user_count int not null
+            );
+            """
+            )
 
-       # uncomment this to test the output 
-        # print(query)
-        # print(count)
-        # sys.exit() 
+            conn.execute(query)
+            query = text("""
+            INSERT INTO aggregate_user 
+            SELECT message->'$.entityType' as entity_type, count(*) as count
+            FROM data
+            WHERE message->'$.clientId' = :client_id and session_id = :session_id and message->'$.entityType' is not null
+            group by entity_type;
 
-    return count
+            """
+            )
+
+            conn.execute(query,{"session_id":session_id, "client_id":client_id})
+    except:
+        return False
+
+    # uncomment this to test the output 
+    # query = text("""
+    # SELECT * 
+    # FROM aggregate_user;
+    # """
+    # )
+    # result = conn.execute(query)
+    # count = [r[1:] for r in result]
+    # sum = np.sum(count)
+    # print(sum == 50510)
+    # sys.exit() 
+
+    return True
 
 
 def process_file(id, file):
@@ -124,8 +180,19 @@ if __name__ == "__main__":
 
     # infinite poll & process
     while True:
-        aggregate_user(126,5) 
-        aggregate_interaction_type(126, 4) 
+        # will be moving to unit test files
+        # user_flag = aggregate_user(126,5) 
+        # if user_flag:
+        #     print("User aggregation succeeded!")
+        # else:
+        #     print("User aggregation failed!")
+
+        # interaction_flag = aggregate_interaction_type(126, 1) 
+        # if interaction_flag:
+        #     print("Interaction aggregation succeeded!")
+        # else:
+        #     print("Interaction aggregation failed!")
+
         ready = check_for_unprocessed_captures()
         if len(ready) > 0:
             print("Ready to process:", ready)
